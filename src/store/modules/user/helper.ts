77d990101e511d218748e42354de420d68b0a95a
Panguangchao <guangchao.pan@easystack.cn>
2023-03-23 14:54:23 +0800

    Modify information

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >Github</a>',
+      avatar: 'https://s2.loli.net/2023/03/23/RNulLkbUyhaGXV7.png',
+      name: '2333',
+      description: 'Chatbot',
     },
   }
 }
